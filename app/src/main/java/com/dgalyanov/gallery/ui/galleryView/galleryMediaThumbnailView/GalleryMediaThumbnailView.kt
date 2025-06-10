d694ae0bf61ea5b2d5c525fb0dd31ae480ac5e39
DmitriyGalyanov <64410709+DmitriyGalyanov@users.noreply.github.com>
2025-06-10 17:48:12 +0300

    Show Duration only on Videos' Thumbnails (and style it)

--- app/src/main/java/com/dgalyanov/gallery/ui/galleryView/galleryMediaThumbnailView/GalleryMediaThumbnailView.kt
+++ app/src/main/java/com/dgalyanov/gallery/ui/galleryView/galleryMediaThumbnailView/GalleryMediaThumbnailView.kt
@@ -1,17 +1,26 @@
 package com.dgalyanov.gallery.ui.galleryView.galleryMediaThumbnailView
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.dgalyanov.gallery.GalleryViewModel
 import com.dgalyanov.gallery.galleryContentResolver.dataClasses.GalleryMediaItem
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.DurationUnit
 
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
@@ -34,7 +43,17 @@ internal fun GalleryMediaThumbnailView(
 
     GalleryMediaThumbnailSelectionIndicator(item.selectionIndex)
 
-    Text(item.durationMs.toString())
+    if (item.durationMs > 0) {
+      Box(
+        modifier = Modifier
+          .clip(RoundedCornerShape(2.dp))
+          .align(Alignment.BottomEnd)
+          .background(Color(0, 0, 0, 120))
+          .padding(horizontal = 2.dp)
+      ) {
+        Text(item.durationMs.milliseconds.toString(DurationUnit.SECONDS))
+      }
+    }
   }
 }
 
