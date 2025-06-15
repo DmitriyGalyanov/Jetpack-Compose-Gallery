79500809496812fb6a2a05d1fac8463d063c0bca
DmitriyGalyanov <64410709+DmitriyGalyanov@users.noreply.github.com>
2025-06-15 17:58:39 +0300

    Update GalleryLogFactory.kt

--- app/src/main/java/com/dgalyanov/gallery/utils/GalleryLogFactory.kt
+++ app/src/main/java/com/dgalyanov/gallery/utils/GalleryLogFactory.kt
@@ -4,12 +4,16 @@ import android.util.Log
 
 internal const val GALLERY_BASE_LOG_TAG = "Gallery"
 
+private const val DEBUG = false
+
 @Suppress("FunctionName")
 internal fun GalleryLogFactory(
   tag: String,
   messagePrefix: String = "",
 ): (message: String) -> Unit {
+  // todo: use message getter
   return fun(message: String) {
+    if (!DEBUG) return
     // long Tags cause Formatting Problems in Studio's Logcat
     Log.d("${GALLERY_BASE_LOG_TAG}_$tag", "$messagePrefix $message")
   }
