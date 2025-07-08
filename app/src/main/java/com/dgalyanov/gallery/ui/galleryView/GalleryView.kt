fddd4f04f8ebf83fbcfcded8831d9c6fc88167b5
DmitriyGalyanov <64410709+DmitriyGalyanov@users.noreply.github.com>
2025-07-08 17:04:57 +0300

    add explicit Background Color to GalleryView

--- app/src/main/java/com/dgalyanov/gallery/ui/galleryView/GalleryView.kt
+++ app/src/main/java/com/dgalyanov/gallery/ui/galleryView/GalleryView.kt
@@ -1,10 +1,12 @@
 package com.dgalyanov.gallery.ui.galleryView
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.dgalyanov.gallery.galleryViewModel.GalleryViewModel
 import com.dgalyanov.gallery.ui.commonViews.FillingLoaderView
 import com.dgalyanov.gallery.ui.galleryView.galleryViewContent.GalleryViewContent
@@ -16,7 +18,7 @@ internal fun GalleryView(
   val shouldShowLoader =
     galleryViewModel.isFetchingAllAssets || galleryViewModel.isPreparingSelectedAssetsForEmission
 
-  Box(modifier = Modifier.fillMaxSize()) {
+  Box(modifier = Modifier.fillMaxSize().background(Color.Black)) {
     FillingLoaderView(shouldShowLoader)
 
     Column(modifier = Modifier.fillMaxSize()) {
