ad84d2b5f6e6a040fdee40da982931b3e49a02cf
DmitriyGalyanov <64410709+DmitriyGalyanov@users.noreply.github.com>
2025-07-11 18:55:54 +0800

    replace Multiselect-Toggle's Text with Icons

--- app/src/main/java/com/dgalyanov/gallery/ui/galleryView/galleryViewContent/galleryViewToolbar/GalleryViewToolbar.kt
+++ app/src/main/java/com/dgalyanov/gallery/ui/galleryView/galleryViewContent/galleryViewToolbar/GalleryViewToolbar.kt
@@ -3,6 +3,7 @@ package com.dgalyanov.gallery.ui.galleryView.galleryViewContent.galleryViewToolb
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -10,16 +11,20 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.dgalyanov.gallery.R
 import com.dgalyanov.gallery.galleryViewModel.GalleryViewModel
 import com.dgalyanov.gallery.ui.galleryView.galleryViewContent.galleryViewToolbar.galleryAlbumsSheetButton.GalleryAlbumsSheetButton
+import com.dgalyanov.gallery.ui.theme.withCoercedFontScaleForNonText
 import com.dgalyanov.gallery.ui.theme.withCoercedFontScaleForText
 
 internal val GALLERY_VIEW_TOOLBAR_HEIGHT = 48.dp
@@ -69,14 +74,18 @@ internal fun GalleryViewToolbar() {
       }
     }
 
+    val multiselectEnabledIcon = painterResource(R.drawable.multiselect_enabled)
+    val multiselectDisabledIcon = painterResource(R.drawable.multiselect_disabled)
     AnimatedVisibility(
       visible = !isSelectingDrafts,
       enter = fadeIn(),
       exit = fadeOut(),
     ) {
-      Text(
-        text = "Multiselect: ${if (isMultiselectEnabled) "enabled" else "disabled"}",
+      Image(
+        contentDescription = if (isMultiselectEnabled) "Multiselect is enabled" else "Multiselect is disabled",
+        painter = if (isMultiselectEnabled) multiselectEnabledIcon else multiselectDisabledIcon,
         modifier = Modifier
+          .size(24.withCoercedFontScaleForNonText())
           .clickable(onClick = galleryViewModel::toggleIsMultiselectEnabled)
       )
     }
