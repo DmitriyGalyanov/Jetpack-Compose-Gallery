2e1a96f3916fdb6aa26bf76a8f3184a016dd87b6
DmitriyGalyanov <64410709+DmitriyGalyanov@users.noreply.github.com>
2025-07-14 15:14:08 +0800

    replace Text with Icon in AssetThumbnailSelectionIndicator

--- app/src/main/java/com/dgalyanov/gallery/ui/galleryView/galleryViewContent/assetThumbnailView/AssetThumbnailSelectionIndicator.kt
+++ app/src/main/java/com/dgalyanov/gallery/ui/galleryView/galleryViewContent/assetThumbnailView/AssetThumbnailSelectionIndicator.kt
@@ -5,8 +5,10 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
@@ -16,17 +18,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.dgalyanov.gallery.R
 import com.dgalyanov.gallery.dataClasses.GalleryAsset
 import com.dgalyanov.gallery.galleryViewModel.GalleryViewModel
 import com.dgalyanov.gallery.ui.theme.withCoercedFontScaleForNonText
 import com.dgalyanov.gallery.ui.theme.withCoercedFontScaleForText
 import com.dgalyanov.gallery.ui.utils.modifiers.conditional
 
-private const val WRAP_SIZE = 18
+private const val WRAP_SIZE = 20
 
 @Composable
 internal fun AssetThumbnailSelectionIndicator(selectionIndex: Int) {
@@ -42,22 +46,25 @@ internal fun AssetThumbnailSelectionIndicator(selectionIndex: Int) {
     Box(
       modifier = Modifier
         .align(Alignment.TopEnd)
-        .offset((-8).dp, 8.dp)
+        .offset((-6).dp, 6.dp)
         .clip(CircleShape)
         .conditional(isMultiselectEnabled) { border(2.dp, Color.White, CircleShape) }
         .size(WRAP_SIZE.withCoercedFontScaleForNonText())
-        .conditional(isSelected) { background(Color.White) }
+        .conditional(isSelected && isMultiselectEnabled) { background(Color.White) }
     ) {
       if (isSelected) {
-        Text(
-          // todo: use Icon instead of "✓"
-          text = if (isMultiselectEnabled) (selectionIndex + 1).toString() else "✓",
+        if (isMultiselectEnabled) Text(
+          text = (selectionIndex + 1).toString(),
           modifier = Modifier.fillMaxSize(),
           textAlign = TextAlign.Center,
-          fontSize = 10.withCoercedFontScaleForText(),
+          fontSize = 11.withCoercedFontScaleForText(),
           fontWeight = FontWeight.Black,
           lineHeight = WRAP_SIZE.withCoercedFontScaleForText(),
           color = Color.Black,
+        ) else Icon(
+          contentDescription = "Transparent checkmark in an accent-colored circle (this asset is selected in non-multiselect mode)",
+          painter = painterResource(R.drawable.single_asset_selection_indicator),
+          modifier = Modifier.requiredSize((WRAP_SIZE + 4).withCoercedFontScaleForNonText()),
         )
       }
     }
