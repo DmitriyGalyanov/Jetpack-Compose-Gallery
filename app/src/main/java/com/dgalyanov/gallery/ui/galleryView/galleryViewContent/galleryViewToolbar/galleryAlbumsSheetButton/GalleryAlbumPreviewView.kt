1442c7d73a39695d7465bce9cc240ac0319e5cb6
DmitriyGalyanov <64410709+DmitriyGalyanov@users.noreply.github.com>
2025-07-14 15:27:32 +0800

    add Placeholders and Scaling to GalleryAlbumPreviewView

--- app/src/main/java/com/dgalyanov/gallery/ui/galleryView/galleryViewContent/galleryViewToolbar/galleryAlbumsSheetButton/GalleryAlbumPreviewView.kt
+++ app/src/main/java/com/dgalyanov/gallery/ui/galleryView/galleryViewContent/galleryViewToolbar/galleryAlbumsSheetButton/GalleryAlbumPreviewView.kt
@@ -1,39 +1,41 @@
 package com.dgalyanov.gallery.ui.galleryView.galleryViewContent.galleryViewToolbar.galleryAlbumsSheetButton
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.dgalyanov.gallery.dataClasses.GalleryAssetsAlbum
+import com.dgalyanov.gallery.ui.theme.withCoercedFontScaleForNonText
 
-
-private val ALBUM_PREVIEW_IMAGE_SIZE = 80.dp
+private const val ALBUM_PREVIEW_IMAGE_SIZE = 80
 
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 internal fun GalleryAlbumPreviewView(album: GalleryAssetsAlbum, onClick: () -> Unit) {
   Row(
-    verticalAlignment = Alignment.CenterVertically,
     modifier = Modifier
       .fillMaxWidth()
-      .clickable(onClick = onClick)
+      .clickable(onClick = onClick),
+    verticalAlignment = Alignment.CenterVertically,
+    horizontalArrangement = Arrangement.spacedBy(16.dp),
   ) {
-    // todo: add placeholder
     album.previewUri?.let {
       GlideImage(
         it,
@@ -41,14 +43,20 @@ internal fun GalleryAlbumPreviewView(album: GalleryAssetsAlbum, onClick: () -> U
         contentScale = ContentScale.Crop,
         modifier = Modifier
           .clip(RoundedCornerShape(8.dp))
-          .size(ALBUM_PREVIEW_IMAGE_SIZE)
+          .size(ALBUM_PREVIEW_IMAGE_SIZE.withCoercedFontScaleForNonText())
       )
-    }
+    } ?: Box(
+      modifier = Modifier
+        .clip(RoundedCornerShape(8.dp))
+        .background(Color.DarkGray)
+        .size(ALBUM_PREVIEW_IMAGE_SIZE.withCoercedFontScaleForNonText())
+    )
 
-    Spacer(Modifier.width(8.dp))
-
-    Column(modifier = Modifier.fillMaxHeight()) {
-      Text(album.name, fontWeight = FontWeight.Medium)
+    Column(
+      modifier = Modifier.fillMaxHeight(),
+      verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+      Text(album.name.ifEmpty { "Unnamed" }, fontWeight = FontWeight.Medium)
       Text(album.assetsAmount.toString())
     }
   }
