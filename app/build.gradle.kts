a286483aaf2757477655b587ae9fa714f80d6c6e
DmitriyGalyanov <64410709+DmitriyGalyanov@users.noreply.github.com>
2025-07-14 17:54:41 +0800

    remove currentWindowContainerSize

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,7 +70,6 @@ dependencies {
   implementation(libs.androidx.ui)
   implementation(libs.androidx.ui.graphics)
   implementation(libs.androidx.material3)
-  implementation(libs.androidx.window)
   implementation(libs.camera.view)
   testImplementation(libs.junit)
   androidTestImplementation(libs.androidx.junit)
